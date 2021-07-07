2b1ceb8b849ce80ecac694a8f7a3ae210a05b0e4
sumanth prabhu <sumanthprabhu2000@gmail.com>
2021-07-07 17:39:52 +0530

    FirstCommit

---
+++ gaze_tracking/eye.py
@@ -0,0 +1,90 @@
+import math
+import numpy as np
+import cv2
+from .pupil import Pupil
+
+
+class Eye(object):
+    """
+    Detects the iris and estimates the position of the iris by calculating the centroid.
+    :param eye_frame (numpy.ndarray): Frame containing an eye and nothing else
+    """
+
+    LEFT_EYE_POINTS = [36, 37, 38, 39, 40, 41]
+    RIGHT_EYE_POINTS = [42, 43, 44, 45, 46, 47]
+
+    def __init__(self, original_frame, landmarks, side, calibration):
+        self.frame = None
+        self.origin = None
+        self.center = None
+        self.pupil = None
+
+        self._analyze(original_frame, landmarks, side, calibration)
+
+    @staticmethod
+    def _middle_point(p1, p2):
+        x = int((p1.x + p2.x) / 2)
+        y = int((p1.y + p2.y) / 2)
+        return x, y
+
+    def _isolate(self, frame, landmarks, points):
+        # put the six landmark coordinates for the eye into an array
+        region = np.array([(landmarks.part(point).x, landmarks.part(point).y) for point in points])
+        region = region.astype(np.int32)
+
+        # Applying a mask to get only the eye
+        height, width = frame.shape[:2]
+        # black array the size of the webcam image
+        black_frame = np.zeros((height, width), np.uint8)
+        # white array the size of the webcam image
+        mask = np.full((height, width), 255, np.uint8)
+        # in the white mask, fill eye shape (contour given by landmarks) with black
+        cv2.fillPoly(mask, [region], (0, 0, 0))
+        # keep only the eye in the webcam image copy (par: src, dst, mask)
+        eye = cv2.bitwise_not(black_frame, frame.copy(), mask=mask)
+
+        # Cropping on the eye
+        margin = 5
+        min_x = np.min(region[:, 0]) - margin
+        max_x = np.max(region[:, 0]) + margin
+        min_y = np.min(region[:, 1]) - margin
+        max_y = np.max(region[:, 1]) + margin
+
+        self.frame = eye[min_y:max_y, min_x:max_x]
+        self.origin = (min_x, min_y)
+
+        height, width = self.frame.shape[:2]
+        self.center = (width / 2, height / 2)
+
+    def _blinking_ratio(self, landmarks, points):
+        left = (landmarks.part(points[0]).x, landmarks.part(points[0]).y)
+        right = (landmarks.part(points[3]).x, landmarks.part(points[3]).y)
+        top = self._middle_point(landmarks.part(points[1]), landmarks.part(points[2]))
+        bottom = self._middle_point(landmarks.part(points[5]), landmarks.part(points[4]))
+
+        eye_width = math.hypot((left[0] - right[0]), (left[1] - right[1]))
+        eye_height = math.hypot((top[0] - bottom[0]), (top[1] - bottom[1]))
+
+        try:
+            ratio = eye_width / eye_height
+        except ZeroDivisionError:
+            ratio = None
+
+        return ratio
+
+    def _analyze(self, original_frame, landmarks, side, calibration):
+        if side == 0:
+            points = self.LEFT_EYE_POINTS
+        elif side == 1:
+            points = self.RIGHT_EYE_POINTS
+        else:
+            return
+
+        self.blinking = self._blinking_ratio(landmarks, points)
+        self._isolate(original_frame, landmarks, points)
+
+        if not calibration.is_complete():
+            calibration.evaluate(self.frame, side)
+
+        threshold = calibration.threshold(side)
+        self.pupil = Pupil(self.frame, threshold)
---
+++ gaze_tracking/gaze_tracking.py
@@ -0,0 +1,136 @@
+from __future__ import division
+import os
+
+import cv2
+import dlib
+from .eye import Eye
+
+
+class GazeTracking(object):
+   
+    def __init__(self, iris_calibration):
+
+        self.frame = None
+        self.eye_left = None
+        self.eye_right = None
+        self.calibration = iris_calibration
+
+        # _face_detector is used to detect faces
+        self._face_detector = dlib.get_frontal_face_detector()
+
+        # _predictor is used to get facial landmarks of a given face
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        model_path = os.path.abspath(os.path.join(cwd, "trained_models/shape_predictor_68_face_landmarks.dat"))
+        self._predictor = dlib.shape_predictor(model_path)
+
+    @property
+    def pupils_located(self):
+        """Checks that the pupils have been located"""
+        try:
+            int(self.eye_left.pupil.x)
+            int(self.eye_left.pupil.y)
+            int(self.eye_right.pupil.x)
+            int(self.eye_right.pupil.y)
+            return True
+        except Exception:
+            return False
+
+    def _analyze(self):
+        """Detects the face and initializes Eye objects"""
+        frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        faces = self._face_detector(frame)
+
+        try:
+            landmarks = self._predictor(frame, faces[0])
+            self.eye_left = Eye(frame, landmarks, 0, self.calibration)
+            self.eye_right = Eye(frame, landmarks, 1, self.calibration)
+
+        except IndexError:
+            self.eye_left = None
+            self.eye_right = None
+
+    def refresh(self, frame):
+
+        self.frame = frame
+        self._analyze()
+
+    def pupil_left_coords(self):
+        """Returns the coordinates of the left pupil"""
+        if self.pupils_located:
+            x = self.eye_left.origin[0] + self.eye_left.pupil.x
+            y = self.eye_left.origin[1] + self.eye_left.pupil.y
+            return x, y
+
+    def pupil_right_coords(self):
+        """Returns the coordinates of the right pupil"""
+        if self.pupils_located:
+            x = self.eye_right.origin[0] + self.eye_right.pupil.x
+            y = self.eye_right.origin[1] + self.eye_right.pupil.y
+            return x, y
+
+    def horizontal_ratio(self):
+        if self.pupils_located:
+            # Remove padding (5 px) from both the eye coord and pupil coord to get
+            # the two actual distances from eye corner. Then divide to obtain relative position
+            # of pupil wrt eye width.
+            pupil_left = self.eye_left.pupil.x / (2 * (self.eye_left.center[0] - 5))
+            pupil_right = self.eye_right.pupil.x / (2 * (self.eye_right.center[0] - 5))
+            return (pupil_left + pupil_right) / 2
+
+    def vertical_ratio(self):
+
+        if self.pupils_located:
+            pupil_left = self.eye_left.pupil.y / (2 * (self.eye_left.center[1] - 5))
+            pupil_right = self.eye_right.pupil.y / (2 * (self.eye_right.center[1] - 5))
+            return (pupil_left + pupil_right) / 2
+
+    def is_right(self):
+        """Returns true if the user is looking to the right"""
+        if self.pupils_located:
+            return self.horizontal_ratio() <= 0.35
+
+    def is_left(self):
+        """Returns true if the user is looking to the left"""
+        if self.pupils_located:
+            return self.horizontal_ratio() >= 0.65
+
+    def is_center(self):
+        """Returns true if the user is looking to the center"""
+        if self.pupils_located:
+            return self.is_right() is not True and self.is_left() is not True
+
+    def is_up(self):
+        """Returns true if the user is looking to the right"""
+        if self.pupils_located:
+            return self.vertical_ratio() <= 0.35
+
+    def is_down(self):
+        """Returns true if the user is looking to the left"""
+        if self.pupils_located:
+            return self.vertical_ratio() >= 0.65
+
+    def is_level(self):
+        """Returns true if the user is looking to the center"""
+        if self.pupils_located:
+            return self.is_up() is not True and self.is_down() is not True
+
+    def is_blinking(self):
+        """Returns true if the user closes his eyes. Is based on eye_width / eye_height"""
+        if self.pupils_located:
+            blinking_ratio = (self.eye_left.blinking + self.eye_right.blinking) / 2
+            return blinking_ratio > 5  # 3.8 original val
+
+    def annotated_frame(self):
+        """Returns the main frame with pupils marked with a green cross"""
+        frame = self.frame.copy()
+
+        if self.pupils_located:
+            color = (0, 255, 0)
+            x_left, y_left = self.pupil_left_coords()
+            x_right, y_right = self.pupil_right_coords()
+            cv2.line(frame, (x_left - 5, y_left), (x_left + 5, y_left), color)
+            cv2.line(frame, (x_left, y_left - 5), (x_left, y_left + 5), color)
+            cv2.line(frame, (x_right - 5, y_right), (x_right + 5, y_right), color)
+            cv2.line(frame, (x_right, y_right - 5), (x_right, y_right + 5), color)
+
+        return frame
